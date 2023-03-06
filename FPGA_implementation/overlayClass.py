2a550a0d4176de666aa24be7e71c64965c45a7bf
Edly <70310901+edlyIrsyad@users.noreply.github.com>
2023-03-06 14:36:14 +0800

    Finetuned overlayClass.py, should work

--- FPGA_implementation/overlayClass.py
+++ FPGA_implementation/overlayClass.py
@@ -10,30 +10,36 @@ dma = overlay.axi_dma_0
 
 class OL():
     def overlay(input):
-        # allocate in and out buffer
+        # Allocate in and out buffer
+
+        # In buffer of 6 floats
         in_buffer = pynq.allocate(shape=(6,), dtype=np.float32)
 
-        # out buffer of 1 integer
+        # Out buffer of 1 integer
         out_buffer = pynq.allocate(shape=(1,), dtype=np.int32)
 
-        # fill in buffer with data populate with array of [0.01, 0.02, .., 0.24]
-        # input_data = np.arange(0.01, 0.25, 0.01, dtype=np.double)
-
+        # 1st number of the input is the Player ID (i.e. 1 or 2), 2nd to 7th numbers are to be fed into the neural network
         for i, val in enumerate(input):
-            if(i != 0):
+            if(i == 0):
+                player_id = val
+            else:
                 in_buffer[i-1] = val
 
+        print(player_id)
         print(in_buffer)
 
-        # dma send and receive channel transfer
+        # DMA send and receive channel transfer
         dma.sendchannel.transfer(in_buffer)
         dma.recvchannel.transfer(out_buffer)
 
-        # wait for transfer to finish
+        # Wait for transfer to finish
         dma.sendchannel.wait()
         dma.recvchannel.wait()
 
-        # print output buffer
-        for output in out_buffer:
-            print(output)
+        # Print and return output buffer
+        # Should be a list of 2 elements:
+        #   1) Player ID
+        #   2) Identified action (by neural network)
+        output = [player_id, out_buffer[0]]
+        print(output)
         return output
