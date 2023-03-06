64d6ebf08738cfeb72cd41346f7360daad9e8f1d
Edly <70310901+edlyIrsyad@users.noreply.github.com>
2023-03-06 14:01:00 +0800

    FInetuned original overlayClass.py template code to fit our implementation

--- FPGA_implementation/overlayClass.py
+++ FPGA_implementation/overlayClass.py
@@ -11,16 +11,17 @@ dma = overlay.axi_dma_0
 class OL():
     def overlay(input):
         # allocate in and out buffer
-        in_buffer = pynq.allocate(shape=(24,), dtype=np.double)
+        in_buffer = pynq.allocate(shape=(6,), dtype=np.float32)
 
         # out buffer of 1 integer
         out_buffer = pynq.allocate(shape=(1,), dtype=np.int32)
 
         # fill in buffer with data populate with array of [0.01, 0.02, .., 0.24]
-        input_data = np.arange(0.01, 0.25, 0.01, dtype=np.double)
+        # input_data = np.arange(0.01, 0.25, 0.01, dtype=np.double)
 
-        for i, val in enumerate(input_data):
-            in_buffer[i] = val
+        for i, val in enumerate(input):
+            if(i != 0):
+                in_buffer[i-1] = val
 
         print(in_buffer)
 
@@ -30,6 +31,7 @@ class OL():
 
         # wait for transfer to finish
         dma.sendchannel.wait()
+        dma.recvchannel.wait()
 
         # print output buffer
         for output in out_buffer:
