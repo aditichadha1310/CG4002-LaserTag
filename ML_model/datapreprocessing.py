10e23620110478a8e064b61bb463ff20b8c28c4d
Edly <70310901+edlyIrsyad@users.noreply.github.com>
2023-03-07 20:11:58 +0800

    Added template code for creating a processed data csv file

--- ML_model/datapreprocessing.py
+++ ML_model/datapreprocessing.py
@@ -1,4 +1,21 @@
 import pandas as pd
+import numpy as np
 
 # Read dataset for training and testing
 data = pd.read_csv("ML_model\dataset.csv")
+
+# Create an example dictionary
+users = {'Name': ['Amit', 'Cody', 'Drew'],
+    'Age': [20,21,25]}
+ 
+# Create DataFrame
+df = pd.DataFrame(users, columns=['Name','Age'])
+ 
+print("Original DataFrame:")
+print(df)
+ 
+df.to_csv('datapreprocessed.csv')
+new_df = pd.read_csv('datapreprocessed.csv')
+
+print('Data from Users.csv:')
+print(new_df)
