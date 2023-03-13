b1dd69e02b0b155e3c6814858820cb9a343a4437
Edly <70310901+edlyIrsyad@users.noreply.github.com>
2023-03-13 16:28:20 +0800

    Made new virtual env

--- ML_model/datapreprocessing.py
+++ ML_model/datapreprocessing.py
@@ -2,20 +2,24 @@ import pandas as pd
 import numpy as np
 
 # Read dataset for training and testing
-data = pd.read_csv("ML_model\dataset.csv")
+data = pd.read_csv("dataset2.csv")
 
-# Create an example dictionary
-users = {'Name': ['Amit', 'Cody', 'Drew'],
-    'Age': [20,21,25]}
- 
-# Create DataFrame
-df = pd.DataFrame(users, columns=['Name','Age'])
- 
-print("Original DataFrame:")
-print(df)
- 
-df.to_csv('datapreprocessed.csv')
-new_df = pd.read_csv('datapreprocessed.csv')
+print(data.head())
+data = data.drop(columns=['date','time','username','wrist','activity'])
+print(data.head())
 
-print('Data from Users.csv:')
-print(new_df)
+acc_data = data[['acceleration_x','acceleration_y','acceleration_z']].values
+gyro_data = data[['gyro_x','gyro_y','gyro_z']].values
+
+data['acc_mean'] = np.mean(acc_data, axis=1)
+data['acc_std'] = np.std(acc_data, axis=1)
+data['acc_max'] = np.max(acc_data, axis=1)
+data['acc_min'] = np.min(acc_data, axis=1)
+
+data['gyro_mean'] = np.mean(gyro_data, axis=1)
+data['gyro_std'] = np.std(gyro_data, axis=1)
+data['gyro_max'] = np.max(gyro_data, axis=1)
+data['gyro_min'] = np.min(gyro_data, axis=1)
+
+print(data.head())
+data.to_csv('datapreprocessed.csv')
