fb70ea14fde1c95b8197715c149a4231d5a5533f
Edly <70310901+edlyIrsyad@users.noreply.github.com>
2023-03-15 12:26:33 +0800

    Commited

--- ML_model/test/run_test.py
+++ ML_model/test/run_test.py
@@ -3,10 +3,7 @@ import time
 import numpy as np
 import start_of_move_test2
 
-min = 0
-isChanged = False
-
-with open('final_data.csv') as csv_file:
+with open('test/lzh_total_data2.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
     window = []
     # window = np.array(window)
@@ -15,6 +12,9 @@ with open('final_data.csv') as csv_file:
     next(csv_reader)
     counter = 0
     change = [0,0]
+
+    min = 0
+    isChanged = False
 
     for row in csv_reader:
         # Process the row here
@@ -26,27 +26,27 @@ with open('final_data.csv') as csv_file:
         change[1] = action
         if change[1]-change[0]!=0:
             isChanged = True
-        print(action,end=" ")
         counter += 1
         # print(counter,end=" ")
-        if counter > 99 and action != 0:
+        if counter > 90 and action != 0:
             print()
             print("------------------------------Grenade-------------------------------")
             counter = -50
+        print(action,end=" ")
         nparr = nparr[0:3]
         window.append(nparr)
         if len(window) == 10:
             output = start_of_move_test2.detect_movement_start(window)
             if output < min:
                 min = output
-            output = str(output)
+            # output = str(output)
             # with open('output.csv','a') as out:
             #     out.write(str(action))
             #     out.write(",")
             #     out.write(output)
             #     out.write('\n')
             if isChanged:
-                with open('output.csv','a') as out:
+                with open('test/output.csv','a') as out:
                     out.write(str(change[0]))
                     out.write(",")
                     out.write(str(min))
