3d6a18c700cf38e8f648d690800adbd03a7ce1e8
Edly <70310901+edlyIrsyad@users.noreply.github.com>
2023-03-20 13:11:37 +0800

    Complete initial run files, now need to test

--- hardware_ai/new/run/external_comms_sim.py
+++ hardware_ai/new/run/external_comms_sim.py
@@ -12,3 +12,30 @@
 #   - Compress 20 rows into 1 row
 
 # Pass the 1 row of 100 columns into the overlay function
+
+import time
+import csv
+from start_of_move_checker import StartOfMoveClass
+import feature_engineering
+import overlay
+import numpy as np
+
+checker = StartOfMoveClass()
+
+with open('/Users/edly/Documents/GitHub/CG4002-LaserTag/hardware_ai/new/datasets/17032023_20Hz.csv') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    # Skip the header row if present
+    next(csv_reader)
+
+    for row in csv_reader:
+        np_arr = np.array(row)
+        output = checker.input(np_arr)
+        if output != 0:
+            output.append(np_arr)
+            if len(output) == 20:
+                engineered_features = feature_engineering(output)
+                action = overlay(engineered_features)
+
+        # Delay for 0.05 s --> 20 Hz sampling rate
+        time.sleep(0.05)
--- hardware_ai/new/run/start_of_move_checker.py
+++ hardware_ai/new/run/start_of_move_checker.py
@@ -1,11 +1,36 @@
+from statistics import mean
+
 class StartOfMoveClass:
+    WINDOW_SIZE = 12
+    WINDOW_HALF = 6
 
     def __init__(self):
         self.window = []
-        WINDOW_SIZE = 12
-        WINDOW_HALF = 6
         
     def input(self, data):
         self.window.append(data)
-        if len(window) == WINDOW_SIZE:
+        if len(self.window) == self.WINDOW_SIZE:
+            isPossibleMove = self.start_of_move_check(self.window)
+            return isPossibleMove
+        else:
+            return 0
 
+    def start_of_move_check(self,window):
+        accX = []
+        accY = []
+        accZ = []
+        for row in window:
+            accX.append(float(row[0]))
+            accY.append(float(row[1]))
+            accZ.append(float(row[2]))
+
+        diffX = mean(accX[self.WINDOW_HALF:]) - mean(accX[0:self.WINDOW_HALF])
+        diffY = mean(accY[self.WINDOW_HALF:]) - mean(accY[0:self.WINDOW_HALF])
+        diffZ = mean(accZ[self.WINDOW_HALF:]) - mean(accZ[0:self.WINDOW_HALF])
+
+        mean_acc_change = diffX + diffY + diffZ
+
+        if mean_acc_change > 10 or mean_acc_change < -10:
+            return window
+        else:
+            return 0
