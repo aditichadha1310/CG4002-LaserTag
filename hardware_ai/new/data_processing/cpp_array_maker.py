cde4f67e6e4b172517cab697f1ffb2ef39724b30
Edly Irsyad bin Elham <70310901+edlyIrsyad@users.noreply.github.com>
2023-03-26 15:53:28 +0800

    Convert weights and biases into CPP array format, and test CPP code

---
+++ hardware_ai/new/data_processing/cpp_array_maker.py
@@ -0,0 +1,16 @@
+with open("C:/Users/edly1/Documents/GitHub/CG4002-LaserTag/Output_Biases3.txt", "r") as text:
+    text_data = text.readlines()
+    print(type(text_data))
+    for index, word in enumerate(text_data):
+        word = word.strip()
+        if "," in word:
+            result = "{" + word
+            if index == len(text_data)-1:
+                result += "}"
+            else:
+                result += "},"
+        else:
+            result = word
+            if index != len(text_data)-1:
+                result += ","
+        print(result, end="\n")
